576709d00067e100a81c71cfc5c207252eb49ee3
Unknown <unknown@example.com>
2020-12-12 14:34:10 -0500

    +ClaimsKeys

---
+++ src/main/java/com/jhw/module/admin/seguridad/permission/ClaimsKeys.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.module.admin.seguridad.permission;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class ClaimsKeys {
+
+    public static final String ACCESS_LEVEL_KEY = "access_level";
+    public static final String PRIMARY_KEY_KEY = "pk";
+}
