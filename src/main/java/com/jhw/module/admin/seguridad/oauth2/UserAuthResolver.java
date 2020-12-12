7c267e9e1f63740b14f10b87be6fada479d83ae2
Unknown <unknown@example.com>
2020-12-12 15:50:34 -0500

    Rename: JwtUserResolver => UserAuthResolver

--- src/main/java/com/jhw/module/admin/seguridad/oauth2/UserAuthResolver.java
+++ src/main/java/com/jhw/module/admin/seguridad/oauth2/UserAuthResolver.java
@@ -9,6 +9,7 @@ import com.jhw.module.admin.seguridad.core.domain.UsuarioDomain;
 import com.jhw.module.admin.seguridad.core.usecase_def.UsuarioUseCase;
 import com.jhw.module.admin.seguridad.permission.ClaimsKeys;
 import com.jhw.module.admin.seguridad.rest.A_ModuleAdminSeguridad;
+import com.jhw.utils.services.ConverterService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -16,7 +17,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
-public class JwtUserResolver {
+public class UserAuthResolver {
 
     private final static UsuarioUseCase usuarioUC = A_ModuleAdminSeguridad.usuarioUC;
 
@@ -31,7 +32,7 @@ public class JwtUserResolver {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             org.springframework.security.oauth2.jwt.Jwt jwt = (org.springframework.security.oauth2.jwt.Jwt) auth.getPrincipal();
             if (jwt.containsClaim(ClaimsKeys.ACCESS_LEVEL_KEY)) {
-                return jwt.getClaim(ClaimsKeys.ACCESS_LEVEL_KEY);
+                return ConverterService.convert((Object) jwt.getClaim(ClaimsKeys.ACCESS_LEVEL_KEY), Integer.class);
             } else {
                 UsuarioDomain user = _resolveUser(jwt);
                 return user.getRolFk().getNivelAcceso();
@@ -43,7 +44,8 @@ public class JwtUserResolver {
 
     private static UsuarioDomain _resolveUser(org.springframework.security.oauth2.jwt.Jwt jwt) throws Exception {
         if (jwt.containsClaim(ClaimsKeys.PRIMARY_KEY_KEY)) {//lo busco por la llave
-            return usuarioUC.findBy(jwt.getClaim(ClaimsKeys.PRIMARY_KEY_KEY));
+            Integer pk = ConverterService.convert((Object) jwt.getClaim(ClaimsKeys.PRIMARY_KEY_KEY), Integer.class);
+            return usuarioUC.findBy(pk);
         } else {//lo busco por el nombre de usuario
             String username = jwt.getClaim("user_name");//llave para el nombre de usuario por defecto de oauth2
             return usuarioUC.loadUserByUsername(username);
