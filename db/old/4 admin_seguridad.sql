e9cbb98c6d2a233d458d04d56032676b8bc89870
Unknown <unknown@example.com>
2021-01-01 13:35:46 -0500

    Exportada DB con root101_

---
+++ db/old/4 admin_seguridad.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:7733
+-- Generation Time: Jan 01, 2021 at 07:29 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admin_seguridad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre_cliente` varchar(100) NOT NULL,
+  `secret` varchar(64) NOT NULL,
+  `redirectURI` varchar(100) NOT NULL,
+  `tokenValidationSeconds` int(11) NOT NULL,
+  `refreshTokenValidationSeconds` int(11) NOT NULL,
+  `descripcion` varchar(500) NOT NULL,
+  `grant_types_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nombre_cliente`, `secret`, `redirectURI`, `tokenValidationSeconds`, `refreshTokenValidationSeconds`, `descripcion`, `grant_types_fk`) VALUES
+(1, 'client1', '$2a$10$Yb8dkozATOxs098B0TcgmuDJQ7HeVVLQAgRAImzRgfifyXN2uHTzC', 'http://localhost:9090', 43200, 43200, '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grant_type`
+--
+
+CREATE TABLE `grant_type` (
+  `id_grant_type` int(11) NOT NULL,
+  `nombre_grant_types` varchar(50) NOT NULL,
+  `descripcion` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `grant_type`
+--
+
+INSERT INTO `grant_type` (`id_grant_type`, `nombre_grant_types`, `descripcion`) VALUES
+(1, 'password, refresh_token', 'Password con refresh_token'),
+(2, 'authorization_code', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` int(11) NOT NULL,
+  `nombre_rol` varchar(100) NOT NULL,
+  `nivel_acceso` int(11) NOT NULL,
+  `descripcion` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rol`
+--
+
+INSERT INTO `rol` (`id_rol`, `nombre_rol`, `nivel_acceso`, `descripcion`) VALUES
+(1, 'VISITANTE', 1, ''),
+(2, 'USUARIO', 3, ''),
+(3, 'ADMIN', 5, ''),
+(4, 'SUPER-ADMIN', 10, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `rol_fk` int(11) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id_user`, `username`, `email`, `password`, `rol_fk`, `descripcion`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2a$10$zIKF8VhNupV5BEBqAnksf.OExvQCNJJmQxbhKqN4nDkAXHSo4qOCW', 3, 'asdasdasdasdasd'),
+(6, '123', '123@gmail.com', '$2a$10$mA8hsn8l7xv1mHpWeBauo.AFM9d.c5dDtXn/WhtbAdd06rEG/yZWS', 1, '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD UNIQUE KEY `nombre_cliente` (`nombre_cliente`),
+  ADD KEY `FK_cliente_grant_types` (`grant_types_fk`);
+
+--
+-- Indexes for table `grant_type`
+--
+ALTER TABLE `grant_type`
+  ADD PRIMARY KEY (`id_grant_type`),
+  ADD UNIQUE KEY `name_grant_types` (`nombre_grant_types`);
+
+--
+-- Indexes for table `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`),
+  ADD UNIQUE KEY `nombre_rol` (`nombre_rol`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `FK_user_rol` (`rol_fk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `grant_type`
+--
+ALTER TABLE `grant_type`
+  MODIFY `id_grant_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `FK_cliente_grant_types` FOREIGN KEY (`grant_types_fk`) REFERENCES `grant_type` (`id_grant_type`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `FK_user_rol` FOREIGN KEY (`rol_fk`) REFERENCES `rol` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
